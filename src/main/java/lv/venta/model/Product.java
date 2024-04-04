88729999dce794145af20a30ab4f6f91e7a6d29f
SwagSek <lolinz666@gmail.com>
2024-04-04 16:11:22 +0300

    Add: validation

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,10 @@
 package lv.venta.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,9 +20,22 @@ public class Product {
 	@Setter(value = AccessLevel.NONE) //No automatic set for ID
 	private int id;
 	
+	@NotNull
+	@Size(min = 3, max = 20)
+	@Pattern(regexp = "[A-Z]{1}[a-z ]+")
 	private String title;
+	
+	@NotNull
+	@Size(min = 3, max = 200)
+	@Pattern(regexp = "[A-Za-z .:!]+")
 	private String description;
+	
+	@Min(0)
+	@Max(10000)
 	private float price;
+	
+	@Min(0)
+	@Max(10000)
 	private int quantity;
 	
 	private static int counter = 1;
@@ -25,12 +43,16 @@ public class Product {
 	public void setID() {
 		this.id = counter++;
 	}
-	
-	public Product(String title, String description, float price, int quantity) {
+
+	public Product(@NotNull @Size(min = 3, max = 20) @Pattern(regexp = "[A-Z]{1}[a-z ]+") String title,
+			@NotNull @Size(min = 3, max = 200) @Pattern(regexp = "[A-Za-z .:!]+") String description,
+			@Min(0) @Max(10000) float price, @Min(0) @Max(10000) int quantity) {
 		setID();
-		setTitle(title);
-		setDescription(description);
-		setPrice(price);
-		setQuantity(quantity);
+		this.title = title;
+		this.description = description;
+		this.price = price;
+		this.quantity = quantity;
 	}
+	
+	
 }
