1d1c31353442ddea9c8bf73c5c4d2c3c1077261b
SwagSek <lolinz666@gmail.com>
2024-04-11 15:12:29 +0300

    Update: Implemented crudservice for firstcontroller

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -3,23 +3,26 @@ package lv.venta.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lv.venta.model.Product;
+import lv.venta.service.ICRUDProductService;
+import lv.venta.service.IFilterProductService;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class FirstController {
 	
-	/*
-	private Product p1 = new Product("Eggs", "Chicken eggs", 3.00f, 16);
-	private Product p2 = new Product("Apples", "Sour Green Apples", 1.50f, 8);
-	private Product p3 = new Product("Cola", "Coca-cola", 1.30f, 20);
-	private ArrayList<Product> productList = new ArrayList<Product>(Arrays.asList(p1, p2, p3));
-	*/
+	@Autowired
+	private ICRUDProductService crudService;
+	
+	@Autowired
+	private IFilterProductService filterService;
 	
 	@GetMapping("/hello") //localhost:8080/hello
 	public String getHello() {
@@ -33,38 +36,39 @@ public class FirstController {
 		model.addAttribute("mydata", "This is a test message.");
 		return "hello-msg-page"; 
 	}
-	@GetMapping("/product")
+	@GetMapping("/product/test")
 	public String getProduct(Model model) {
-		model.addAttribute("product", new Product("Eggs", "Chicken eggs", 3.00f, 16));
-		return "product-page";
+		try {
+			model.addAttribute("product", crudService.retrieveById(1));
+			return "product-page";
+		} 
+		catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
+			return "error-page";
+		}
 	}
 	
 	@GetMapping("/product/getList")
 	public String getProductList(Model model) {
-		
-	
-		model.addAttribute("productList", productList);
-		return "productList-page";
+		try {
+			model.addAttribute("productList", crudService.retrieveAll());
+			return "productList-page";
+		} 
+		catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
+			return "error-page";
+		}
 	}
 	
 	@GetMapping("/product/one") //localhost:8080/product/one?id=x
 	public String getProductOneId(@RequestParam("id")int id, Model model) {
-		if(id >= 0) {
-			for (Product p : productList) {
-				if(p.getId() == id) {
-					model.addAttribute("product", p);
-					return "product-page";
-				}
-			}
-			model.addAttribute("errormsg", "Product is not found.");
+		try {
+			model.addAttribute("product", crudService.retrieveById(id));
+			return "product-page";
+		}
+		catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
 			return "error-page";
 		}
-		else {
-			model.addAttribute("errormsg", "Expected positive ID.");
-			return "error-page";
-		}
-	}
-	
-	
-	
+	}	
 }
