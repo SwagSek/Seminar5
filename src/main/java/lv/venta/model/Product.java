d1f0304594e7e1640f5eab58193bbaa55e86def5
SwagSek <lolinz666@gmail.com>
2024-04-18 15:34:09 +0300

    New: added data JPA annotations in model class

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,12 @@
 package lv.venta.model;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -15,39 +22,41 @@ import lombok.ToString;
 @Setter
 @ToString
 @NoArgsConstructor
+@Table(name = "ProductTable")
+@Entity
 public class Product {
 	
 	@Setter(value = AccessLevel.NONE) //No automatic set for ID
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	@NotNull
 	@Size(min = 3, max = 20)
 	@Pattern(regexp = "[A-Z]{1}[a-z ]+", message = "Only letters allowed")
+	@Column(name = "Title")
 	private String title;
 	
 	@NotNull
 	@Size(min = 3, max = 200)
 	@Pattern(regexp = "[A-Za-z .:!]+")
+	@Column(name = "Description")
 	private String description;
 	
 	@Min(0)
 	@Max(10000)
+	@Column(name = "Price")
 	private float price;
 	
 	@Min(0)
 	@Max(10000)
+	@Column(name = "Quantity")
 	private int quantity;
 	
-	private static int counter = 1;
-	
-	public void setID() {
-		this.id = counter++;
-	}
-
 	public Product(@NotNull @Size(min = 3, max = 20) @Pattern(regexp = "[A-Z]{1}[a-z ]+") String title,
 			@NotNull @Size(min = 3, max = 200) @Pattern(regexp = "[A-Za-z .:!]+") String description,
 			@Min(0) @Max(10000) float price, @Min(0) @Max(10000) int quantity) {
-		setID();
 		this.title = title;
 		this.description = description;
 		this.price = price;
