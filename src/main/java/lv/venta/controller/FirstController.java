5ca16ad26e5293165f3b2133c9b6f8b9744c4894
SwagSek <lolinz666@gmail.com>
2024-04-18 14:41:00 +0300

    Add: getProductFilterByPrice, quantity, title or desc, total value

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -60,6 +60,7 @@ public class FirstController {
 	public String getProductList(Model model) {
 		try {
 			model.addAttribute("productList", crudService.retrieveAll());
+			model.addAttribute("msg", "All products");
 			return "productList-page";
 		} 
 		catch (Exception e) {
@@ -142,7 +143,65 @@ public class FirstController {
 	public String getProductDeleteById(@PathVariable("id") int id, Model model) {
 		try {
 			crudService.deleteById(id);
-			return "redirect:/product/getList";
+			model.addAttribute("productList", crudService.retrieveAll());
+			//Refrain from using redirect: on get.
+			return "productList-page";
+		} 
+		catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	@GetMapping("product/filter/price/{threshold}")
+	public String getProductFilterByPrice(@PathVariable("threshold") float threshold, Model model) {
+		try {
+			ArrayList<Product> filteredProducts = filterService.filterByPriceLessThanThreshold(threshold);
+			model.addAttribute("productList", filteredProducts);
+			model.addAttribute("msg", "Products filtered by price: " + threshold + " eur");
+			return "productList-page";
+		} 
+		catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	@GetMapping("product/filter/quantity/{threshold}")
+	public String getProductFilterByQuantity(@PathVariable("threshold") int threshold, Model model) {
+		try {
+			ArrayList<Product> filteredProducts = filterService.filterByQuantityLessThanThreshold(threshold);
+			model.addAttribute("productList", filteredProducts);
+			model.addAttribute("msg", "Products filtered by quantity: " + threshold + " items");
+			return "productList-page";
+		} 
+		catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	@GetMapping("product/filter/titleOrDesc/{text}")
+	public String getProductFilterByTitleOrDescription(@PathVariable("text") String text, Model model) {
+		try {
+			ArrayList<Product> filteredProducts = filterService.filterByTitleOrDescription(text);
+			model.addAttribute("productList", filteredProducts);
+			model.addAttribute("msg", "Products filtered by title or description by string: " + text);
+			return "productList-page";
+		} 
+		catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	@GetMapping("product/filter/totalValue")
+	public String getProductTotalValue(Model model) {
+		try {
+			ArrayList<Product> allProducts = crudService.retrieveAll();
+			model.addAttribute("productList", allProducts);
+			model.addAttribute("msg", "Total sum of all products: " + filterService.calculateProductsTotalValue());
+			return "productList-page";
 		} 
 		catch (Exception e) {
 			model.addAttribute("errormsg", e.getMessage());
